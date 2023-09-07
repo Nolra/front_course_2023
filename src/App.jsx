c0a97467d9261b412eae0ac43f671c020073b6f7
Vladimir Esionov <nolraprof@gmail.com>
2023-09-07 21:49:39 +0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import "./global.css"
+import ChatRoom from "./components/ChatRoom";
+
+
+function App() {
+  return (
+      <>
+        <ChatRoom />
+      </>
+  );
+}
+
+
+export default App;
+
+
